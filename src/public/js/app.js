779d9863ca78e3997e62f0b928b32dce120db66a
jingom368 <jingom369@gmail.com>
2023-11-28 17:23:30 +0900

    23.11.28 dinner SOOM.js

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -7,7 +7,24 @@
 //     예를 들어 socket.send(data)를 사용하면 서버에 데이터를 보낼 수 있고, 
 //     socket.onmessage = function(event) {...}를 사용하면 서버로부터 데이터를 받을 수 있습니다.
 
+const nickForm = document.querySelector("#nick")
+const messageForm = document.querySelector("#message");
+const messageList = document.querySelector("ul")
+
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+
+// function makeMessage(type, payload) {...}: makeMessage라는 이름의 함수를 선언하고, 이 함수는 type과 payload라는 두 개의 매개변수를 받습니다.
+// const msg = {type, payload};: msg라는 이름의 상수를 선언하고, 이 상수에는 type과 payload로 구성된 객체를 할당합니다. 
+//     이 때, 객체의 속성 이름과 값이 같은 변수의 경우, ES6 문법에서는 속성 이름을 생략하고 변수만 적을 수 있습니다. 따라서 {type: type, payload: payload}를 {type, payload}로 줄여 쓴 것입니다.
+// return JSON.stringify(msg);: msg 객체를 JSON 문자열로 변환한 후, 그 결과를 반환합니다. JSON.stringify는 자바스크립트 객체를 JSON 형식의 문자열로 변환하는 메서드입니다.
+// 따라서 이 함수는 type과 payload라는 두 개의 값을 받아서, 이 두 값으로 구성된 객체를 JSON 문자열로 변환한 후 반환하는 역할을 합니다. 
+//     주로 서버와 클라이언트 간에 데이터를 주고받을 때, 데이터를 JSON 형식으로 변환해서 보내는 데 사용됩니다.
+
+function makeMessage(type, payload) {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
 
 
 // socket.addEventListener("open", () => {...}) : WebSocket 연결이 성공적으로 열렸을 때 실행되는 이벤트 리스너를 설정합니다. 
@@ -22,7 +39,11 @@ socket.addEventListener("open", () => {
 })
 
 socket.addEventListener("message", (message) => {
-    console.log("방금 받은 메시지: ", message);
+    // console.log("방금 받은 메시지: ", message); // 서버에서 받은 메세지
+    // console.log("방금 받은 메시지: ", message.data); // 서버에서 받은 메세지 "안녕?"
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.appendChild(li);
 })
 
 socket.addEventListener("close", () => {
@@ -33,6 +54,45 @@ socket.addEventListener("close", () => {
 // 주어진 코드는 JavaScript의 setTimeout 함수를 사용하여 3초 후에 실행되는 코드입니다. socket.send 함수를 사용하여 "브라우저가 인사해 안녕?" 메시지를 보내고 있습니다.
 // 이 코드는 웹 소켓을 사용하여 클라이언트와 서버간에 실시간 통신을 수행하는 것으로 추측됩니다. 3초 후에 클라이언트는 서버에게 "브라우저가 인사해 안녕?" 메시지를 보내게 됩니다.
 
-setTimeout(() => {
-    socket.send("브라우저가 인사해 안녕?");
-}, 3000);
+// 시간차로 메세지 주고 받기
+/*
+    setTimeout(() => {
+        socket.send("브라우저가 인사해 안녕?"); // 서버로 보내는 메세지
+    }, 3000);
+*/
+
+
+// handleSubmit(event) 함수 & handleNickSubmit(event) 함수
+// 이 함수는 메시지 전송 폼에서 'submit' 이벤트가 발생했을 때 실행됩니다.
+// event.preventDefault()는 폼 제출에 의한 페이지 새로고침을 방지합니다.
+// messageForm.querySelector("input")을 통해 메시지 입력창의 값을 가져옵니다.
+// socket.send(makeMessage("new_message", input.value))를 통해 웹소켓을 통해 서버에 메시지를 전송합니다. "new_message"는 메시지 타입을 나타냅니다.
+// &
+// socket.send(makeMessage("nickname", input.value))를 통해 웹소켓을 통해 서버에 닉네임 변경 요청을 전송합니다. "nickname"은 메시지 타입을 나타냅니다.
+// 마지막으로 input.value = ""를 통해 닉네임 입력창을 비웁니다.
+
+// messageForm.addEventListener("submit", handleSubmit)와 nickForm.addEventListener("submit", handleNickSubmit)는 각각 메시지 전송 폼과 닉네임 변경 폼에서 
+// 'submit' 이벤트가 발생하면 위에서 설명한 함수들이 실행되도록 이벤트 리스너를 등록하는 코드입니다.
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    // socket.send(input.value);
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    // socket.send(input.value);
+    // socket.send({
+    //     type: "nickname",
+    //     payload: input.value
+    // });
+    socket.send(makeMessage("nickname", input.value));
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -60,22 +60,64 @@ const wss = new WebSocket.Server({server});
 
 
 // wss.on("connection", (socket) => {...}); : 이 부분은 wss WebSocket 서버에서 "connection" 이벤트가 발생했을 때, 
-//     즉 클라이언트가 서버에 연결되었을 때 실행되는 콜백 함수를 설정하는 부분입니다. 연결된 클라이언트의 소켓 객체는 socket 파라미터로 전달됩니다.
-// console.log("브라우저와 연결되었습니다."); : 클라이언트와 연결이 성공하면 콘솔에 "브라우저와 연결되었습니다."라는 메시지를 출력합니다.
-// socket.on("close", () => console.log("서버 > 서버와 연결이 끊겼습니다.")); : 클라이언트와의 연결이 닫혔을 때 실행되는 콜백 함수를 설정합니다. 
-//     이 경우 연결이 닫히면 콘솔에 "서버 > 서버와 연결이 끊겼습니다."라는 메시지를 출력합니다.
-// socket.on("message", (message) => {...}) : 클라이언트로부터 메시지를 받았을 때 실행되는 콜백 함수를 설정합니다. 이 경우 메시지를 받으면 받은 메시지를 콘솔에 출력합니다.
-// socket.send("안녕?"); : 클라이언트와 연결이 성공하면 클라이언트에게 "안녕?" 메시지를 보냅니다. 
+// 즉 클라이언트가 서버에 연결되었을 때 실행되는 콜백 함수를 설정하는 부분입니다. 연결된 클라이언트의 소켓 객체는 socket 파라미터로 전달됩니다.
+    // "console.log("socket", socket)"는 "socket"라는 문자열과 socket 객체를 별개로 출력합니다. 이 방식을 사용하면 socket 객체의 내용을 자세히 볼 수 있습니다.
+    // "console.log("socket" + socket)"는 "socket"라는 문자열과 socket 객체를 문자열 연결 연산자(+)를 사용하여 하나의 문자열로 만들어 출력합니다. 
+    //     이 경우, socket 객체가 문자열로 변환되어 출력되므로, [object Object]와 같이 출력될 수 있습니다.
+    // 따라서, 객체의 내용을 자세히 보고 싶다면 "console.log("socke", socket)" 방식을, 객체를 문자열과 함께 한 줄로 출력하고 싶다면 "console.log("socke" + socket)" 방식을 사용하면 됩니다.
+    // sockets.push(socket): 새로 연결된 웹소켓을 sockets 배열에 추가합니다.
+    // console.log("브라우저와 연결되었습니다."); : 클라이언트와 연결이 성공하면 콘솔에 "브라우저와 연결되었습니다."라는 메시지를 출력합니다.
+    // socket.on("close", () => console.log("서버 > 서버와 연결이 끊겼습니다.")); : 클라이언트와의 연결이 닫혔을 때 실행되는 콜백 함수를 설정합니다. 
+    //     이 경우 연결이 닫히면 콘솔에 "서버 > 서버와 연결이 끊겼습니다."라는 메시지를 출력합니다.
+    // socket.on("message", (message) => {...}) : 클라이언트로부터 메시지를 받았을 때 실행되는 콜백 함수를 설정합니다. 이 경우 메시지를 받으면 받은 메시지를 콘솔에 출력합니다.
+        // const message = JSON.parse(msg): 클라이언트에서 보낸 메시지를 JSON형식으로 파싱합니다.
+        // console.log([서버] : ${message});: 이 코드는 서버 측에서 실행되며, 서버의 콘솔에 "[서버] : "라는 문자열과 함께 클라이언트로부터 받은 메시지를 출력합니다. 
+        // 이는 주로 개발자가 서버에서 클라이언트로부터 어떤 메시지를 받았는지 확인하기 위해 사용됩니다.
+        // socket.send([서버] : ${message});: 이 코드는 서버가 클라이언트로 메시지를 보내는 코드입니다. "[서버] : "라는 문자열과 함께 클라이언트로부터 받은 메시지를 그대로 클라이언트에게 보냅니다. 
+        // 이는 웹소켓을 통해 실시간으로 서버와 클라이언트 간의 데이터를 주고받는 데 사용됩니다.
+        // sockets.forEach(aSocket => aSocket.send(${message})): sockets 배열에 있는 모든 웹소켓에 메시지를 보냅니다. 이를 통해 서버는 받은 메시지를 모든 클라이언트에게 브로드캐스팅합니다.
+
+        // switch(message.type) {...} : message.type의 값에 따라 다른 코드 블록이 실행됩니다. message는 클라이언트에서 서버로 보낸 메시지를 JSON 형태로 파싱한 객체입니다.
+        // case "new_message": : 메시지의 타입이 "new_message"인 경우 다음 코드를 실행합니다.
+        // sockets.forEach(aSocket => aSocket.send(${socket.nickname}:${message.payload})); : 현재 연결된 모든 소켓에 메시지를 전송합니다. 
+        // 메시지는 보낸 사람의 닉네임과 메시지 내용을 합쳐서 전송됩니다. 여기서 socket.nickname은 메시지를 보낸 사람의 닉네임이고, message.payload는 메시지 내용입니다.
+        // case "nickname": : 메시지의 타입이 "nickname"인 경우 다음 코드를 실행합니다.
+        // socket["nickname"] = message.payload; : 메시지를 보낸 사람의 닉네임을 message.payload로 변경합니다.
+        // break; : switch 문을 빠져나옵니다. 이 키워드가 없으면, 해당 case 블록 이후의 코드도 실행됩니다.
+        // 즉, 클라이언트에서 메시지 타입이 "new_message"로 보내면, 해당 메시지를 서버에 연결된 모든 클라이언트에게 전송하고, 
+        // 메시지 타입이 "nickname"인 경우, 메시지를 보낸 사람의 닉네임을 변경하는 작업을 수행합니다.
+   
+    // socket.send("안녕?"); : 클라이언트와 연결이 성공하면 클라이언트에게 "안녕?" 메시지를 보냅니다. 
 
 // server.listen(3000, handelListen); : HTTP 서버를 3000번 포트에서 시작하고, 리스닝을 시작하면 handelListen 함수를 호출합니다.
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+    console.log("socket", socket);
+    console.log("socket" + socket);
+    sockets.push(socket);
+    socket["nickname"] = "익명"; 
     console.log("브라우저와 연결되었습니다.");
     socket.on("close", () => console.log("서버 > 서버와 연결이 끊겼습니다."));
-    socket.on("message", (message) => {
-        console.log(`${message}`);
+    socket.on("message", (msg) => { 
+        const message = JSON.parse(msg);
+        console.log(message.type, message.payload); // 클라이언트에서 받은 JSON 타입의 key값을 받아서 value를 반환
+        // console.log(`[서버] : ${message}`); // 클라이언트에서 받은 메세지
+        // socket.send(`[서버] : ${message}`); // 클라이언트에서 받은 메세지를 클라이언트로 보내는 메시지
+        // sockets.forEach(aSocket => aSocket.send(`${message}`)); // 클라이언트에서 받은 메세지를 소켓에 연결된 모든 클라이언트로 보내는 메세지
+
+        switch(message.type) {
+            case "new_message":
+                // sockets.forEach(aSocket => aSocket.send(`${message.payload}`));
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}:${message.payload}`));
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload
+                break;                
+        }
     })
-    socket.send("안녕?");
+    // socket.send("안녕?"); // 클라이언트로 보내는 메세지
 });
 
 server.listen(3000, handelListen);
